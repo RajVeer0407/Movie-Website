433025bca0a08b279865aa4d49746e27f5f94f33
RajVeer0407 <er.rajveershaw@gmail.com>
2024-06-28 11:52:46 +0530

    Added Trending

--- src/components/Trending/TrendingTv.tsx
+++ src/components/Trending/TrendingTv.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import useTrendingList from '@/hooks/useTrendingList'
+import React, { useState } from 'react'
+import { MdOutlineExpandMore } from 'react-icons/md';
+import MovieCard from '../MovieCard';
+import TvShowCard from '../TvShowCard';
 
 const TrendingTv = () => {
+    const {trendingData} = useTrendingList("tv");
+    const [expand, setExpand] = useState(false);
+    console.log(trendingData)
   return (
-    <div>TrendingTv</div>
+    <div className='p-3 mb-4'>
+        <div className='flex items-center cursor-pointer' onClick={()=>setExpand(!expand)}>
+            <h1 className='text-4xl font-semibold p-5 py-8'>Trending Tv</h1>
+            <span className='text-4xl text-gray-500'>
+                <MdOutlineExpandMore/>
+            </span>
+        </div>
+        <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid:cols-2 gap-y-3">
+            {expand && trendingData?.map((tvshow)=>(
+                <div key={tvshow.id}>
+                    <TvShowCard tvShowResult={tvshow}/>
+                </div>
+            ))}
+            {!expand && trendingData?.slice(0, 10).map((tvshow)=>(
+                <div key={tvshow.id}>
+                    <TvShowCard tvShowResult={tvshow}/>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
